05fa9ee27c6a5dac14cd6cd877f42c8a98c6e314
BlueAxolotLArseniy <131585010+BlueAxolotLArseniy@users.noreply.github.com>
2025-03-27 18:51:06 +0300

    add window settings

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import Tk
+from tkinter import ttk
+import fuzzywuzzy as fuzz
+import speech_recognition as sr
+
+from window_settings import window_settings
+
+window = Tk()
+window_settings(window)
+
+
+
+window.mainloop()
---
+++ window_settings.py
@@ -0,0 +1,5 @@
+from tkinter import Tk
+
+def window_settings(window: Tk):
+    window.title('VoiceAssistant')
+    window.geometry('250x300')
